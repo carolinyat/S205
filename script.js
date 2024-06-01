f69e721490cc2e548a4447468d63d2bed347719e
Caroliny Abreu <caroliny.abreu@healthbit.com.br>
2024-05-31 22:29:43 -0300

    fiz um dark-mode  e adicionei uma ação ao clicar no ícone de perfil

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 document.getElementById('theme-toggle').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
 });
+
+document.querySelector('.profile-icon img').addEventListener('click', function () {
+    const dropdownMenu = document.querySelector('.profile-icon .dropdown-menu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+});
+
+// Fechar o menu suspenso se clicar fora dele
+document.addEventListener('click', function (event) {
+    const profileIcon = document.querySelector('.profile-icon');
+    const dropdownMenu = document.querySelector('.profile-icon .dropdown-menu');
+    if (!profileIcon.contains(event.target)) {
+        dropdownMenu.style.display = 'none';
+    }
+});
